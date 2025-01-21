b7bfbda0c7deb8597bb22f34edfd62cf1636ed21
Deiby Gerleyn Taborda Giraldo <143036075+DeibyTaborda@users.noreply.github.com>
2025-01-21 16:23:59 -0500

    Rutas para obtener usuarios, contactos y roles

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,9 @@ const loginController = require('../controllers/loginController');
 router.post('/register', userController.registerUser);
 router.post('/create-rol', userController.createRol);
 router.post('/create-contact', userController.createContact);
+router.get('/users', userController.getUsers);
+router.get('/contacts', userController.getContacts);
+router.get('/roles', userController.getRoles);
 router.post('/login', loginController.loguearUser);
 
 module.exports = router;
